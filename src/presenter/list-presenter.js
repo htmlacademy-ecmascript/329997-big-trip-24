10afca3e9b644cfbdcae26b3c1f324d023cca0db
Павел Корольков <roflgan@gmail.com>
2024-09-16 12:11:17 +0700

    1.2 Fixes

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -5,33 +5,40 @@ import PointView from '../view/point-view.js';
 import EditPointView from '../view/edit-point-view.js';
 
 export default class BoardPresenter {
-  listComponent = new ListView();
+  #pointsContainer = null;
+  #pointsModel = null;
+  #offers = [];
+  #destinations = [];
+  #points = [];
+  #blankPoint = {};
+
+  #listComponent = new ListView();
 
   constructor({ pointsContainer, pointsModel }) {
-    this.pointsContainer = pointsContainer;
-    this.pointsModel = pointsModel;
+    this.#pointsContainer = pointsContainer;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
-    this.offers = this.pointsModel.getOffers();
-    this.destinations = this.pointsModel.getDestinations();
-    this.points = this.pointsModel.getPoints().map((point) =>
+    this.#offers = this.#pointsModel.offers;
+    this.#destinations = this.#pointsModel.destinations;
+    this.#points = this.#pointsModel.points.map((point) =>
       ({
         ...point,
-        offersList: this.offers.find((element) => element.type === point.type).offers,
-        destination: this.destinations.find((element) => element.id === point.destination),
+        offersList: this.#offers.find((element) => element.type === point.type).offers,
+        destination: this.#destinations.find((element) => element.id === point.destination),
       }));
     //Blank point>
-    this.blankPoint = this.pointsModel.getBlankPoint();
-    this.blankPoint.offersList = this.offers.find((element) => element.type === this.blankPoint.type).offers;
+    this.#blankPoint = this.#pointsModel.blankPoint;
+    this.#blankPoint.offersList = this.#offers.find((element) => element.type === this.#blankPoint.type).offers;
     //
-    render(new SortView(), this.pointsContainer);
-    render(this.listComponent, this.pointsContainer);
+    render(new SortView(), this.#pointsContainer);
+    render(this.#listComponent, this.#pointsContainer);
     //new point>
-    render(new EditPointView({ point: this.blankPoint, destinations: this.destinations }), this.listComponent.element);
+    render(new EditPointView({ point: this.#blankPoint, destinations: this.#destinations }), this.#listComponent.element);
     //points>
-    this.points.forEach((point) => render(new PointView({ point: point }), this.listComponent.element));
+    this.#points.forEach((point) => render(new PointView({ point: point }), this.#listComponent.element));
     //edit point>
-    render(new EditPointView({ point: this.points[1], destinations: this.destinations }), this.listComponent.element);
+    render(new EditPointView({ point: this.#points[1], destinations: this.#destinations }), this.#listComponent.element);
   }
 }
